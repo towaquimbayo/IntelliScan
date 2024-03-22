343dddaf15c297623b8e75c426f6f70b06bbaac4
Towa Quimbayo <towacurtis@hotmail.com>
2024-03-21 18:55:46 -0700

    added placedholder Admin Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./css/global.css";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="signup" element={<Signup />} />
       <Route path="login" element={<Login />} />
+      <Route path="admin-dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 }
