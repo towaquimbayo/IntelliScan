fe6703ef8b72300f60c59c72637681c6a3133372
noufilsaqib <noufil.s@hotmail.com>
2024-03-30 02:54:59 -0700

    Added validation for Edit User

--- frontend/src/pages/EditUser.js
+++ frontend/src/pages/EditUser.js
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { config } from "../config";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
+import AlertMessage from "../components/AlertMessage";
 
 export default function AdminDashboard() {
   const navigate = useNavigate();
@@ -13,6 +14,7 @@ export default function AdminDashboard() {
   const isAdmin = useSelector((state) => state.user.isAdmin);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
 
   useEffect(() => {
     if (!isLoggedIn) navigate("/login");
@@ -40,6 +42,13 @@ export default function AdminDashboard() {
   const handleUserEdit = async (e, id) => {
     e.preventDefault();
     setLoading(true);
+    setErrMsg("");
+
+    if (!user.name || !user.email || !(user.api_calls >= 0)) {
+      setErrMsg("Please fill in all fields.");
+      setLoading(false);
+      return;
+    }
 
     try {
       const response = await fetch(endpoint + `/api/protected/users/${id}`, {
@@ -55,9 +64,14 @@ export default function AdminDashboard() {
       } else {
         const data = await response.json();
         console.error("Edit user failed:", data);
+        setErrMsg(
+          data.message ||
+          "An unexpected error occurred. Please try again later."
+        );
       }
     } catch (error) {
       console.error("Error during edit user:", error);
+      setErrMsg("An unexpected error occurred. Please try again later.");
     } finally {
       setLoading(false);
     }
@@ -67,55 +81,69 @@ export default function AdminDashboard() {
     <Layout title="Edit User">
       <h1>Edit User</h1>
       {user ? (
-        <form onSubmit={(e) => handleUserEdit(e, user._id)} className="edit-user-form">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              value={user.name}
-              onChange={(e) => setUser({ ...user, name: e.target.value })}
+        <div>
+          {errMsg && <AlertMessage msg={errMsg} type="error" />}
+          <form onSubmit={(e) => handleUserEdit(e, user._id)} className="edit-user-form">
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                value={user.name}
+                onChange={(e) => {
+                  setUser({ ...user, name: e.target.value });
+                  setErrMsg("");
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={user.email}
+                onChange={(e) => {
+                  setUser({ ...user, email: e.target.value });
+                  setErrMsg("");
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="role">Role</label>
+              <select
+                id="role"
+                value={user.admin ? "admin" : "user"}
+                onChange={(e) =>
+                  setUser({ ...user, admin: e.target.value === "admin" })
+                }
+              >
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="apiCalls">API Calls</label>
+              <input
+                type="number"
+                id="apiCalls"
+                min={0}
+                max={20}
+                value={user.api_calls}
+                onChange={(e) => {
+                  setUser({ ...user, api_calls: e.target.value });
+                  setErrMsg("");
+                }}
+              />
+            </div>
+            <Button
+              type="submit"
+              title="Edit"
+              loading={loading}
+              text="Edit"
+              customStyle={{ paddingInline: "2rem" }}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="role">Role</label>
-            <select
-              id="role"
-              value={user.admin ? "admin" : "user"}
-              onChange={(e) =>
-                setUser({ ...user, admin: e.target.value === "admin" })
-              }
-            >
-              <option value="user">User</option>
-              <option value="admin">Admin</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="apiCalls">API Calls</label>
-            <input
-              type="number"
-              id="apiCalls"
-              value={user.api_calls}
-              onChange={(e) => setUser({ ...user, api_calls: e.target.value })}
-            />
-          </div>
-          <Button
-            type="submit"
-            title="Edit"
-            loading={loading}
-            text="Edit"
-            customStyle={{ paddingInline: "2rem" }}
-          />
-        </form>
+          </form>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
